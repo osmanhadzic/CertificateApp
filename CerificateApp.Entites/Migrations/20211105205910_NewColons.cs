d17d22d561a9acfbbe0a473408d024b13af66385
hadzico39 <hadzico39@gmail.com>
2021-11-05 20:59:17 +0100

    pushfirst commit

---
+++ CerificateApp.Entites/Migrations/20211105205910_NewColons.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CerificateApp.Entites.Migrations
+{
+    public partial class NewColons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Plant",
+                table: "User",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "User",
+                type: "varchar(60)",
+                maxLength: 60,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "User");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Plant",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
